9d0f8401f8da384a5c1d3ce606c755dad80b32e3
Lupis <44170316+LupisYoung@users.noreply.github.com>
2022-12-17 15:27:58 +0100

    Add files via upload

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,6 +1,5 @@
 window.onload = randomImage
 
-let bg = document.getElementById('hero-bg')
 let arrow = document.getElementById('subhead')
 
 let header = document.querySelector('header')
@@ -11,8 +10,6 @@ window.addEventListener('scroll', function(){
     let value = window.scrollY;
 
     header.style.top = - value * 0.08 + 'px';
-
-    bg.style.opacity = 100 - value * 0.15 + '%';
     header.style.opacity = 100 - value * 0.15 + '%';
     arrow.style.opacity = 100 - value * 0.8 + '%';
 
